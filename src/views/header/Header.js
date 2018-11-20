4f5b159abf20b9cae2a40b113c53fd180b2b71f4
LiliyaStefanova <liliya.stefanova@gmail.com>
2018-11-20 23:14:59 +0000

    prop types and react strap

--- src/views/header/Header.js
+++ src/views/header/Header.js
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './index.css';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
-import { Link} from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {Navbar, Nav, NavbarBrand, NavLink, NavItem, Collapse} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCompass} from '@fortawesome/free-solid-svg-icons';
+
 library.add(faCompass);
 
-const Header = () =>{
-    return(
-        <Navbar fixedTop>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <FontAwesomeIcon icon="compass"/>
-                    <Link to = '/'>Walk Planner</Link>
-                </Navbar.Brand>
-            </Navbar.Header>
-            <Nav bsStyle="pills">
-                <NavItem><Link to = '/'>Home</Link></NavItem>
-                <NavItem><Link to = 'walk/create'>Create</Link></NavItem>
-                <NavItem><Link to = '/walks/all'>View</Link></NavItem>
-                <NavItem><Link to = '/resources'>Resources</Link></NavItem>
-                <NavItem><Link to = '/contact'>Contact</Link></NavItem>
+export default class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+    };
+
+render()
+    {
+    return (
+        <Navbar expand="md" light color="light">
+            <NavbarBrand className="mr-auto">
+                <FontAwesomeIcon icon="compass"/>
+                <Link to='/'>Walk Planner</Link>
+            </NavbarBrand>
+            <Nav pills>
+                <NavItem>
+                    <NavLink><Link to='/'>Home</Link></NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink><Link to='walk/create'>Create</Link></NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink><Link to='/walks/all'>View</Link></NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink><Link to='/resources'>Resources</Link></NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink><Link to='/contact'>Contact</Link></NavLink>
+                </NavItem>
             </Nav>
+
         </Navbar>
 
-    )
+    );
+    }
 };
 
-export default Header;
+Navbar.propTypes = {
+    light: PropTypes.bool,
+    dark: PropTypes.bool,
+    fixed: PropTypes.string,
+    color: PropTypes.string,
+    role: PropTypes.string,
+    expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+};
+
+NavbarBrand.propTypes = {
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+    // pass in custom element to use
+};
